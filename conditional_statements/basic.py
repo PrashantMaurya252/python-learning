42ce5e2ae6ba6fe55f6ddbcad94fd7336c4d9ef5
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-02-28 22:33:35 +0530

    conditional basics

---
+++ conditional_statements/basic.py
@@ -0,0 +1,38 @@
+# age = input("Povide user age")
+# age_in_int = int(age)
+
+
+# first problem
+# age=76
+# if age < 13:
+#     print("child")
+# elif age <20:
+#     print("teenage")
+# elif age < 60:
+#     print("adult")
+# else:
+#     print('senior')
+
+# 2nd problem
+
+# age = 22
+# day = "Sunday"
+# price = 12 if age >= 18 else 8
+
+# if day == 'Wednesday':
+#     price=price-2
+# print("Ticket price",price)
+
+# 3rd problem
+
+grade=50
+if grade > 90:
+    print('A')
+elif grade > 80:
+    print('B')
+elif grade > 70:
+    print('C')
+elif grade > 60:
+    print('D')
+else:
+    print('F')
