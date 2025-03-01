9d375c212fd76d07256103b31550e997a29e4eee
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-03-01 08:47:30 +0530

    moderate problem on loops

--- mutable.py
+++ mutable.py
@@ -21,8 +21,40 @@
 
 # 3rd Print the multiplication table for a given number upto 10,but skip the fifth iteration
 
-number = 3
-for i in range(1,11):
-    if i==5:
-        continue
-    print(number,'x',i,'=',number*i)
+# number = 3
+# for i in range(1,11):
+#     if i==5:
+#         continue
+#     print(number,'x',i,'=',number*i)
+
+# 4th reverse a string
+
+# input_string='python'
+# reversed_string=''
+
+# for char in input_string:
+#     reversed_string = char + reversed_string
+
+# print(reversed_string)
+
+# 5th first non repeated character in a string
+
+# input_str='teeter'
+
+# for char in input_str:
+#     if input_str.count(char) == 1:
+#         print("Char is: ",char)
+#         break
+
+# 6th factorial number using a while loop
+
+# number = 5
+# factorial=1
+
+# while number > 0:
+#     factorial = factorial * number
+#     number = number-1
+
+# print(factorial)
+
+# 7th keep asking the user for input until they enter a number between 1 and 10.
