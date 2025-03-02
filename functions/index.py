22be6dcb6305f4c2c665d859fb488a26ad28222e
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-03-02 10:33:21 +0530

    python functions

---
+++ functions/index.py
@@ -0,0 +1,36 @@
+# 1st syntax
+# def square(number):
+#     return number**2
+
+# result = square(4)
+# print(result)
+
+
+#  2nd sum of two numbers
+
+# def add(a,b):
+#     return a+b
+
+# print(add(3,5))
+
+# 3rd->Polymorphism->write a function multiplies two numbers,but also accept and multiply strings.
+
+# def multiply(p1,p2):
+#     return p1*p2
+
+# print(multiply(8,5))
+# print(multiply('a',5))
+# print(multiply(5,'a'))
+
+#  4th Create a function that returns both area and circumference of a circle given its radius
+# import math
+
+# def circle(radius):
+#     area=math.pi*radius**2
+#     circumference=2*math.pi*radius
+#     return area,circumference
+
+# a,c=circle(5)
+# print("Area: ",round(a,2),"Circumference: ",round(c,2))
+
+# 5th write a function to greet a user if no name provided then write  default name
